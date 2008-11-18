664d304e7ebbb206df5f52c28b28d6f2e0bb2dc2
matwey <matwey@ffb9f40a-6b89-11dd-a0cb-8391a1d94bf8>
2008-11-18 18:02:56 +0000

    git-svn-id: svn://curl.sai.msu.ru/lsp/trunk@47 ffb9f40a-6b89-11dd-a0cb-8391a1d94bf8

--- least_squares.cpp
+++ least_squares.cpp
@@ -8,24 +8,19 @@
 int main(){
 	using namespace boost::numeric::ublas;
 
-	std::cout << std::numeric_limits< double >::epsilon() << std::endl;
-	std::cout << std::numeric_limits< double >::round_error() << std::endl;
+	unsigned int i,j,m,n;
+	std::cin >> m >> n;
 
-	matrix< double > A0,A(3,3);
-	vector< double > b0,b(3);
+	matrix< double > A( m, n ),A0;
+	vector< double > b( m),b0;
 
+	for( i = 0; i < m; i++ ){
+	for( j = 0; j < n; j++ ){
+		std::cin >> A(i,j);
+	}
+		std::cin >> b(i);
+	}
 
-	b[0]=10.9457;b[1]=57.4715;b[2]=0;
-	A(0,0)=0.0000;
-	A(1,0)=21.4207;
-	A(2,0)=0.0000;
-	A(0,1)=68.9777;
-	A(1,1)=23.3947;
-	A(2,1)=81.6124;
-	A(0,2)=0.0000;
-	A(1,2)=91.3194;
-	A(2,2)=0.0000;
-	
 	std::cout << A.size1() << std::endl;
 
 	std::cout << A << std::endl;
@@ -43,12 +38,6 @@ int main(){
 	std::cout << std::endl;
 	std::cout << A << std::endl;
 	std::cout << b << std::endl;
-
-	for( matrix< double >::const_iterator1 it = A.begin1(); it != A.end1(); ++it ){
-		//for( matrix< double >::const_iterator2 it2 = it.begin(); it2 != it.end(); ++it2 ){
-			std::cerr << *(it.begin()+1) << std::endl;
-		//}
-	}
 
 	return 0;
 }
